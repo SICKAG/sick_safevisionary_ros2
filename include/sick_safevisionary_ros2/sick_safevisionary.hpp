57867454dc10ab07a87dd8b2adcd091d3c5fa18d
Stefan Scherzinger <scherzin@fzi.de>
2023-07-25 00:36:00 +0200

    Add producer-consumer data communication
    We use Boost's single producer single consumer implementation that
    nicely meets our requirements of non-blocking, thread-safe data
    communication.

--- include/sick_safevisionary_ros2/sick_safevisionary.hpp
+++ include/sick_safevisionary_ros2/sick_safevisionary.hpp
@@ -10,11 +10,36 @@
  */
 //-----------------------------------------------------------------------------
 
+#include <boost/lockfree/policies.hpp>
+#include <boost/lockfree/spsc_queue.hpp>
+#include <memory>
+#include <thread>
+
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "sick_safevisionary_base/SafeVisionaryData.h"
+#include "sick_safevisionary_base/SafeVisionaryDataStream.h"
 
 namespace sick
 {
+/**
+ * @brief The primary driver class for the Sick SafeVisionary camera
+ *
+ * This class implements a *lifecycle node* to provide advanced options for managing the driver's internal state.
+ * An overview of these states is given [here](https://design.ros2.org/articles/node_lifecycle.html)
+ *
+ * This driver's behavior is as follows:
+ *
+ * State: Unconfigured  -> Directly after starting the driver. Nothing happens yet.
+ * State: Inactive      -> Connection to the camera established. Data is consumed, but not published to ROS2 yet.
+ * State: Active        -> Data is published to ROS2.
+ * State: Finalized     -> All resources are cleaned up.
+ *
+ * The data communication with the library is based on the *single producer,
+ * single consumer* (spsc) pattern and uses Boost's lock-free, thread-safe buffer to publish
+ * the camera's data to ROS2.
+ *
+ */
 class SickSafeVisionary : public rclcpp_lifecycle::LifecycleNode
 {
 public:
@@ -31,5 +56,13 @@ public:
   CallbackReturn on_cleanup(const rclcpp_lifecycle::State & previous_state) override;
 
   CallbackReturn on_shutdown(const rclcpp_lifecycle::State & previous_state) override;
+
+private:
+  std::shared_ptr<visionary::SafeVisionaryData> data_handle_;
+  std::shared_ptr<visionary::SafeVisionaryDataStream> data_stream_;
+  std::thread receive_thread_;
+  std::thread publish_thread_;
+  boost::lockfree::spsc_queue<visionary::SafeVisionaryData, boost::lockfree::capacity<10>>
+    spsc_queue_;
 };
 }  // namespace sick
--- src/sick_safevisionary.cpp
+++ src/sick_safevisionary.cpp
@@ -12,11 +12,46 @@
 
 #include "sick_safevisionary_ros2/sick_safevisionary.hpp"
 
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+#include "sick_safevisionary_base/SafeVisionaryData.h"
+
 namespace sick
 {
 SickSafeVisionary::CallbackReturn SickSafeVisionary::on_configure(
   [[maybe_unused]] const rclcpp_lifecycle::State & previous_state)
 {
+  data_handle_ = std::make_shared<visionary::SafeVisionaryData>();
+  data_stream_ = std::make_shared<visionary::SafeVisionaryDataStream>(data_handle_);
+
+  // Setup UDP connection to the camera
+  if (!data_stream_->openUdpConnection(htons(6060))) {
+    RCLCPP_ERROR(this->get_logger(), "Could not open UDP connection");
+    return CallbackReturn::ERROR;
+  }
+
+  // Start an asynchronous receive thread with a lock-free producer.
+  receive_thread_ = std::thread([&]() {
+    while (this->get_current_state().label() != "Finalized") {
+      if (!data_stream_->getNextBlobUdp()) {
+        RCLCPP_WARN(this->get_logger(), "UDP stream incomplete, skipping this frame.");
+      }
+      spsc_queue_.push(*data_handle_);
+    }
+  });
+
+  // Start an asynchronous publish thread with a lock-free consumer.
+  // We need to consume regardless of the current state, but publish only if we are active.
+  publish_thread_ = std::thread([&]() {
+    while (this->get_current_state().label() != "Finalized") {
+      auto tmp = visionary::SafeVisionaryData();
+      spsc_queue_.pop(tmp);
+      if (this->get_current_state().label() == "Active") {
+        // TODO: publish
+      }
+    }
+  });
+
   return CallbackReturn::SUCCESS;
 }
 
@@ -41,6 +76,9 @@ SickSafeVisionary::CallbackReturn SickSafeVisionary::on_cleanup(
 SickSafeVisionary::CallbackReturn SickSafeVisionary::on_shutdown(
   [[maybe_unused]] const rclcpp_lifecycle::State & previous_state)
 {
+  receive_thread_.join();
+  publish_thread_.join();
+  data_stream_->closeUdpConnection();
   return CallbackReturn::SUCCESS;
 }
 }  // namespace sick
